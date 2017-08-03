8bcbdeab6186e718a1c82e4dbc5095a36081b6cf
dunhao <dunhao.zhuang@outlook.com>
2017-08-03 22:05:21 +0000

    removed vendor files to make it easy to see.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
 Vue.component('rss-list', require('./components/rss-list.vue'));
 
 const app = new Vue({
--- resources/views/testRssViewVue.blade.php
+++ resources/views/testRssViewVue.blade.php
@@ -20,8 +20,6 @@
 
 <script src="js/jquery-3.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 <script src="js/plugins/axios/axios.min.js"></script>
 <script src="js/app.js"></script>
 </body>
